9a0f394926b58e4506a6dbe3b74e363335099aa9
NoraHille <norahille@web.de>
2021-05-20 14:29:15 +0200

    Basic is there

--- main.py
+++ main.py
@@ -38,18 +38,22 @@ class Agent(object):
         pm = {}
         normalization_faktor = 0;
         for op in issue.options:
+
             dist = self.computeDistance(op)
-            pm[op.name] = dist;
-            normalization_faktor += dist;
+            if(dist == 0):
+                dist = 0.0000000001
+            pref = pow(dist, -1)#raise to the power of -1 to make agents prefer the option with the lowest distance
+            pm[op.name] = pref;
+            normalization_faktor += pref;
         #normaliza PM so it adds up to 1
         sum_of_preferences = 0
-        for (op_name, dist) in pm.items():
-            normalized_pref = dist/normalization_faktor
+        for (op_name, pref) in pm.items():
+            normalized_pref = pref/normalization_faktor
             pm[op_name] = normalized_pref
             sum_of_preferences += normalized_pref
         if(sum_of_preferences != 1):
             print("Something went wrong with the normalization, the normalized value is ", sum_of_preferences)
-        print("The PM of an agent is: ", pm)
+        # print("The PM of an agent is: ", pm)
         return pm
 
     def computeDistance(self, option):
@@ -70,20 +74,25 @@ class Election:
 
     def computeResult(self):
         commom_PM = {}
-        commom_PM.update((op_name, 0) for op_name, pref in self.agents[0].pm.items())
         for op in self.issue.options:
+            commom_PM[op.name] = 0
             for ag in self.agents:
                 commom_PM[op.name] += ag.pm[op.name]
 
-        print(common_PM)
+        print("added up PM: ")
+        for key, value in commom_PM.items():
+            print(key, ' : ', value)
+
         #normalize
         prefs = 0
         for (op_name, pref) in commom_PM.items():
             prefs += pref
         if prefs != len(self.agents):
-            print("Something went wrong with the common PM.")
+            print("Something went wrong with the common PM. Prefs added up to: ", prefs)
         commom_PM.update((op_name, pref/prefs) for op_name, pref in commom_PM.items())
-        print("Normalized PM: ", commom_PM)
+        print("normalized PM: ")
+        for key, value in commom_PM.items():
+            print(key, ' : ', value)
 
 
 
@@ -113,21 +122,42 @@ def initializeRandomElection(numOptions, numAgents, numDimensions):
         agents.append(ag)
     return Election(issue, agents)
 
+def printDict(text, dict):
+    print(text)
+    for key, value in dict.items():
+        print(key, ' : ', value)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
 
+# # Example with a critical result:
+#     op1 = Option([1,2])
+#     op2 = Option([-5, 8])
+#     op3 = Option([5, -4])
+#
+#     issue1 = Issue([op1, op2, op3], ["freedom", "taxes"])
+#     ag1 = Agent([-5,8], issue1)
+#     ag2 = Agent([5,-4], issue1)
+#
+#     # election = initializeRandomElection(5,100,3)
+#     election = Election(issue1, [ag1, ag2])
+#     election.computeResult()
 
-    # op1 = Option([1,2])
-    # op2 = Option([-1, 8])
-    # op3 = Option([5, -4])
-    #
-    # issue1 = Issue([op1, op2, op3], ["freedom", "taxes"])
-    # ag1 = Agent([1,1], issue1)
-    # ag2 = Agent([5,1], issue1)
+# # Example with a good result:
+#     op1 = Option([20,-10])
+#     op2 = Option([-5, 8])
+#     op3 = Option([-5, 6])
 
-    election = initializeRandomElection(3,10,3)
+    issue1 = Issue([op1, op2, op3], ["freedom", "taxes"])
+    ag1 = Agent([10,-8], issue1)
+    ag2 = Agent([-5,7], issue1)
+    ag3 = Agent([-5, 6.5], issue1)
+
+    # election = initializeRandomElection(5,100,3)
+    election = Election(issue1, [ag1, ag2])
     election.computeResult()
+
+
 
 
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
